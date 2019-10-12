5152495db18ded86bf48fd00bea762f01bf8fc5c
Tadas Ivanauskas <ivanauskas15@gmail.com>
2019-10-12 17:58:28 +0200

    Add files via upload
    changed to UDP

--- axis_server.c
+++ axis_server.c
@@ -18,176 +18,110 @@
 #include <capture.h>
 
 #define PORT_NUMBER 1025
-#define SERVER_ADDRESS "192.168.20.249"
+#define SERVER_ADDRESS "192.168.20.252"
 
-void take_image(int peer_socket)
+void take_image(int peer_socket, struct sockaddr_in clientaddr, int clientlen, char **resolution)
 {
 	media_frame *frame;
 	media_stream *stream;
 	void *data;
 	unsigned long long totalbytes = 0;
 	char sizeString[256];
-
-	int remainingData=0;
+	//char resolution = "resolution=352x288&fps=10";
+	stream = capture_open_stream(IMAGE_JPEG, resolution);
 	int sent_bytes;
-	
-	stream = capture_open_stream(IMAGE_JPEG, "resolution=352x288&fps=10");
+
 	frame = capture_get_frame(stream);
 
-	syslog(LOG_INFO,"Frame captured");
-	
 	totalbytes = capture_frame_size(frame);
 	data = capture_frame_data(frame);
 
-	sprintf(sizeString,"%llu\n",totalbytes);
-	syslog(LOG_INFO,"Total bytes: %s",sizeString);
-	
-	
+	sprintf(sizeString, "%llu\n", totalbytes);
+	syslog(LOG_INFO, "Total bytes: %s", sizeString);
+
 	char dataBuffer[totalbytes];
 	memcpy(dataBuffer, data, totalbytes);
 
-	send(peer_socket, sizeString , sizeof(sizeString),0);
-		// sending data
-	sent_bytes = send(peer_socket,dataBuffer,totalbytes,0);
-	syslog(LOG_INFO,"Send %d and remaining: %d Total size: %llu",sent_bytes, remainingData,totalbytes);
+	// sending data
 
-	//send(peer_socket, data, sizeof data,0);
-
-	capture_frame_free(frame);
-	capture_close_stream(stream);
-
-}
-void start_stream(int peer_socket)
-{
-	media_frame *frame;
-	media_stream *stream;
-	void *data;
-	unsigned long long totalbytes = 0;
-	char sizeString[256];
-	char response[32];
-	int remainingData=0;
-	int sent_bytes;
-
-	stream = capture_open_stream(IMAGE_JPEG, "resolution=352x288&fps=10");
-	syslog(LOG_INFO,"Stream started");
-	while (1)
-	{	
-		totalbytes = 0;
-		frame = capture_get_frame(stream);
-		syslog(LOG_INFO,"Frame captured");
-	
-		totalbytes += capture_frame_size(frame);
-		data = capture_frame_data(frame);
-	
-		sprintf(sizeString,"%llu\n",totalbytes);
-		syslog(LOG_INFO,"Total bytes: %s",sizeString);
-		
-		
-//	char dataBuffer[totalbytes];
-//	sprintf(dataBuffer,"%d",data);
-
-		send(peer_socket, sizeString , sizeof(sizeString),0);
-		// sending data
-		sent_bytes = send(peer_socket,data,totalbytes,0);
-		syslog(LOG_INFO,"Send %d and remaining: %d Total size: %llu",sent_bytes, remainingData,totalbytes);
-		
-		//if(recv(peer_socket, command,BUFSIZ,0)>0)
-
-	//send(peer_socket, data, sizeof data,0);
-
-		capture_frame_free(frame);
-		if ((recv(peer_socket, response,32,0)>0)&&strcmp(response,"OK"))
-		{
-			syslog(LOG_INFO,"Client responded: %s",response);
-			break;
-		}
+	sent_bytes = sendto(peer_socket, dataBuffer, totalbytes, 0, (struct sockaddr *)&clientaddr, clientlen);
+	if (sent_bytes < 0)
+	{
+		syslog(LOG_INFO, "ERROR in send_image");
 	}
+	capture_frame_free(frame);
+	memset(&sizeString, 0, 256);
+	memset(&dataBuffer, 0, totalbytes);
 	capture_close_stream(stream);
 }
-
-
-
 
 int main(int argc, char **argv)
 {
-	int server_socket;
-	int peer_socket;
-	socklen_t sock_len;
-	struct sockaddr_in server_addr;
-	struct sockaddr_in peer_addr;
-	char command[BUFSIZ];
-
-	/* Create server socket */
-	server_socket = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_socket == -1)
+	int sockfd;					   /* socket */
+	int clientlen;				   /* byte size of client's address */
+	struct sockaddr_in serveraddr; /* server's addr */
+	struct sockaddr_in clientaddr; /* client addr */
+	char buf[BUFSIZ];			   /* message buf */
+	char *hostaddrp;			   /* dotted decimal host addr string */
+	int optval;					   /* flag value for setsockopt */
+	int n;						   /* message byte size */
+	/* 
+   * socket: create the parent socket 
+   */
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sockfd < 0)
 	{
-		fprintf(stderr, "Error creating socket --> %s", strerror(errno));
-
-		exit(EXIT_FAILURE);
+		syslog(LOG_INFO, "ERROR opening socket");
 	}
+	/* setsockopt: Handy debugging trick that lets 
+   * us rerun the server immediately after we kill it; 
+   * otherwise we have to wait about 20 secs. 
+   * Eliminates "ERROR on binding: Address already in use" error. 
+   */
+	optval = 1;
+	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval, sizeof(int));
 
-	/* Zeroing server_addr struct */
-	memset(&server_addr, 0, sizeof(server_addr));
-	/* Construct server_addr struct */
-	server_addr.sin_family = AF_INET;
-	inet_pton(AF_INET, SERVER_ADDRESS, &(server_addr.sin_addr));
-	server_addr.sin_port = htons(PORT_NUMBER);
+	/*
+   * build the server's Internet address
+   */
+	bzero((char *)&serveraddr, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons((unsigned short)PORT_NUMBER);
 
-	/* Bind */
-	if ((bind(server_socket, (struct sockaddr *)&server_addr, sizeof(struct sockaddr))) == -1)
+	/* 
+   * bind: associate the parent socket with a port 
+   */
+	if (bind(sockfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0)
 	{
-		fprintf(stderr, "Error on bind --> %s", strerror(errno));
-
-		exit(EXIT_FAILURE);
+		syslog(LOG_INFO, "ERROR on binding");
 	}
-
-	/* Listening to incoming connections */
-	if ((listen(server_socket, 5)) == -1)
+	/* 
+   * main loop: wait for a datagram, then echo it
+   */
+	clientlen = sizeof(clientaddr);
+	while (1)
 	{
-		fprintf(stderr, "Error on listen --> %s", strerror(errno));
 
-		exit(EXIT_FAILURE);
-	}
-
-	sock_len = sizeof(struct sockaddr_in);
-	/* Accepting incoming peers */
-	peer_socket = accept(server_socket, (struct sockaddr *)&peer_addr, &sock_len);
-	if (peer_socket == -1)
-	{
-		fprintf(stderr, "Error on accept --> %s", strerror(errno));
-
-		exit(EXIT_FAILURE);
-	}
-	fprintf(stdout, "Accept peer --> %s\n", inet_ntoa(peer_addr.sin_addr));
-	
-	while(1)
-	{
-		memset(&command, 0, sizeof command);
-		if(recv(peer_socket, command,BUFSIZ,0)>0){
-		printf("Received: %s",command);
-		if (!strcmp(command,"send"))
+		/*
+     * recvfrom: receive a UDP datagram from a client
+     */
+		memset(&buf, 0, sizeof buf);
+		n = recvfrom(sockfd, buf, BUFSIZ, 0, (struct sockaddr *)&clientaddr, &clientlen);
+		if (n < 0)
 		{
-			openlog("Logs", LOG_PID, LOG_USER);
-			syslog(LOG_INFO,"Send compare succesfull");
-			take_image(peer_socket);
-			syslog(LOG_INFO,"Image taken");
-		}else if(!strcmp(command,"stream"))
-		{
-			openlog("Logs", LOG_PID, LOG_USER);
-			syslog(LOG_INFO,"stream compare succesfull");
-			start_stream(peer_socket);
-			syslog(LOG_INFO,"Stream ended");
-		}else
-		{
-			syslog(LOG_INFO,"Send compare unsuccesfull. Message received: %s", command);
-			send(peer_socket,command,255,0);	
+			syslog(LOG_INFO, "ERROR in recvfrom");
 		}
+		hostaddrp = inet_ntoa(clientaddr.sin_addr);
+		if (hostaddrp == NULL)
+		{
+			syslog(LOG_INFO, "ERROR on inet_ntoa\n");
+		}
+		if (strstr(buf, "resolution="))
+		{
+			syslog(LOG_INFO, "%s", buf);
+			take_image(sockfd, clientaddr, clientlen, buf);
 		}
 	}
-	closelog();
-
-	close(peer_socket);
-	close(server_socket);
-
 	return 0;
 }
